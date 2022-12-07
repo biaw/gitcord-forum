0c37cd3675d8cf4f39da1ac4057fcf22b54092e6
Glenn <glenn@not-tech.support>
2022-12-07 14:44:58 +0000

    Fix push notifications
    Signed-off-by: GitHub <noreply@github.com>

--- src/handlers/githubEvent.ts
+++ src/handlers/githubEvent.ts
@@ -4,7 +4,7 @@ import { getForumPostThreadIdForRepository } from "./discordForumPost";
 
 export default async function handleGithubEvent(data: WebhookEvent, request: Request): Promise<Response> {
   if ("repository" in data && !(
-    "pusher" in data && data.pusher.name !== "renovate[bot]" ||
+    "pusher" in data && data.pusher.name === "renovate[bot]" ||
     "ref_type" in data && data.ref_type === "branch" && data.ref.startsWith("refs/heads/renovate")
   )) {
     const url = new URL(`${DISCORD_WEBHOOK}/github`);
