674731c8a45eea65e37608cd200363acc3309e60
Promise <me@promise.solutions>
2025-07-18 17:13:18 +0000

    fix: migrate to service worker
    ref. https://developers.cloudflare.com/workers/reference/migrate-to-module-workers/#migrate-a-worker
    
    Signed-off-by: Promise <me@promise.solutions>

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,23 @@ import type { WebhookEvent } from "@octokit/webhooks-types";
 import handleGithubEvent from "./handlers/githubEvent";
 import verifyGithubWebhookSignature from "./utils/verifySignature";
 
-addEventListener("fetch", event => event.respondWith(event.request.method === "POST" ? handlePost(event.request) : Response.redirect(FALLBACK_URL)));
+export default {
+  async fetch(request) {
+    if (request.method === "POST") {
+      const rawGithubSignature = request.headers.get("X-Hub-Signature-256");
+      if (!rawGithubSignature) return new Response(null, { status: 401, statusText: "Signature missing" });
 
-async function handlePost(request: Request): Promise<Response> {
-  const rawGithubSignature = request.headers.get("X-Hub-Signature-256");
-  if (!rawGithubSignature) return new Response(null, { status: 401, statusText: "Signature missing" });
+      const [, githubSignature] = rawGithubSignature.split("=");
+      if (!githubSignature) return new Response(null, { status: 401, statusText: "Signature missing" });
 
-  const [, githubSignature] = rawGithubSignature.split("=");
-  if (!githubSignature) return new Response(null, { status: 401, statusText: "Signature missing" });
+      const payload = await request.clone().text();
+      const isVerified = await verifyGithubWebhookSignature(githubSignature, payload);
+      if (!isVerified) return new Response(null, { status: 401 });
 
-  const payload = await request.clone().text();
-  const isVerified = await verifyGithubWebhookSignature(githubSignature, payload);
-  if (!isVerified) return new Response(null, { status: 401 });
+      const data = await request.clone().json<WebhookEvent>();
+      return handleGithubEvent(data, request);
+    }
 
-  const data = await request.clone().json<WebhookEvent>();
-  return handleGithubEvent(data, request);
-}
+    return Response.redirect(FALLBACK_URL);
+  },
+} satisfies ExportedHandler;
