b3b27a6859cecc94136a9cbd8758907d38e3b67b
Promise <me@promise.solutions>
2025-07-22 14:37:10 +0000

    fix: more descriptive logging
    Signed-off-by: Promise <me@promise.solutions>

--- src/handlers/githubEvent.ts
+++ src/handlers/githubEvent.ts
@@ -1,4 +1,5 @@
 import type { WebhookEvent } from "@octokit/webhooks-types";
+import { type RESTPostAPIWebhookWithTokenGitHubWaitResult, RouteBases, Routes } from "discord-api-types/v10";
 import type Env from "../environment";
 import getForumPostThreadIdForRepository from "./discordForumPost";
 
@@ -8,13 +9,26 @@ export default async function handleGithubEvent(data: WebhookEvent, request: Req
   )) {
     const url = new URL(`${env.DISCORD_WEBHOOK}/github`);
     url.searchParams.set("thread_id", await getForumPostThreadIdForRepository(data.repository, env));
+    url.searchParams.set("wait", "true");
 
-    await fetch(url, {
+    const response = await fetch(url, {
       method: "POST",
       headers: request.headers,
       body: JSON.stringify(await request.clone().json()),
-    });
+    })
+      .then(res => res.json<RESTPostAPIWebhookWithTokenGitHubWaitResult>())
+      .catch((error: unknown) => ({ error }));
+
+    if ("error" in response) {
+      // eslint-disable-next-line no-console
+      console.error("Failed to send Discord webhook:", response.error);
+      return new Response("Failed to send Discord webhook, see Worker logs for more information.", { status: 500 });
+    }
+
+    const messageUrl = new URL(RouteBases.api);
+    messageUrl.pathname = Routes.channelMessage(response.channel_id, response.id);
+    return new Response(`Discord webhook sent successfully - ${messageUrl.toString()}`, { status: 200 });
   }
 
-  return new Response(null, { status: 200 });
+  return new Response("GitHub event ignored", { status: 202 });
 }
